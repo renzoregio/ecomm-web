1aa6e853d14b04afe8c7240adaaa32b9fb5231df
RenzoRegio <renzorregio@gmail.com>
2021-08-08 17:14:13 -0700

    Start Project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
-import { useEffect } from "react";
-import play from "../playground";
+import type { InferGetStaticPropsType } from "next";
+import getAllProducts from "../framework/shopify/products/get-all-products";
 
-export default function Home() {
-  useEffect(() => {
-    play();
-  }, []);
-  return <>Hello World</>;
+export async function getStaticProps() {
+  const products = await getAllProducts();
+  return {
+    props: {
+      products,
+    },
+    revalidate: 4 * 60 * 60,
+  };
+}
+
+export default function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <>{JSON.stringify(products)}</>;
 }
